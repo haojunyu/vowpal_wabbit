947ba483194619806372b96560476ad916a05370
haojunyu <haojunyu2012@gmail.com>
2019-03-25 20:25:55 +0800

    test

--- parse_args.h
+++ parse_args.h
@@ -7,6 +7,7 @@ embodied in the content of this file are licensed under the BSD
 #ifndef PA_H
 #define PA_H
 
+
 void parse_args(int argc, char *argv[], float &eta, float &eta_decay_rate, float &initial_t, 
 		float &power_t, ofstream &predictions, ofstream &raw_predictions, bool &train, 
 		int &numthreads, int& passes, regressor &r, example_file &e,
