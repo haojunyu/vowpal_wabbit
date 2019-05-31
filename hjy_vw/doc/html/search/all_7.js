7f9377f878f17803f4975bba58a0ef7c004c445a
haojunyu <haojunyu2012@gmail.com>
2019-05-31 16:45:52 +0800

    update doc

--- hjy_vw/doc/html/search/all_7.js
+++ hjy_vw/doc/html/search/all_7.js
@@ -1,5 +1,6 @@
 var searchData=
 [
+  ['hash',['hash',['../hash_8cc.html#a5de8c92ebd556dedb75cdd515843edc3',1,'hash(ub1 *k, ub4 length, ub4 initval):&#160;hash.cc'],['../hash_8h.html#a5de8c92ebd556dedb75cdd515843edc3',1,'hash(ub1 *k, ub4 length, ub4 initval):&#160;hash.cc']]],
   ['hash_2ecc',['hash.cc',['../hash_8cc.html',1,'']]],
   ['hash_2eh',['hash.h',['../hash_8h.html',1,'']]],
   ['hashmask',['hashmask',['../hash_8cc.html#aacd6ed104463685849b2550389d05ff3',1,'hash.cc']]],
--- hjy_vw/doc/html/search/functions_7.js
+++ hjy_vw/doc/html/search/functions_7.js
@@ -1,4 +1,5 @@
 var searchData=
 [
+  ['hash',['hash',['../hash_8cc.html#a5de8c92ebd556dedb75cdd515843edc3',1,'hash(ub1 *k, ub4 length, ub4 initval):&#160;hash.cc'],['../hash_8h.html#a5de8c92ebd556dedb75cdd515843edc3',1,'hash(ub1 *k, ub4 length, ub4 initval):&#160;hash.cc']]],
   ['hashstring',['hashstring',['../parse__example_8cc.html#ab7fc146aa0a3985223d538e54a72d0ec',1,'parse_example.cc']]]
 ];
